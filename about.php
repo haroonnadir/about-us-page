8ce24458f3c29acd3984a2a027ce1a87d575a84c
Haroon <biodunhi@gmail.com>
2024-08-20 04:18:16 +0500

    first commit

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./assets/about.css" />
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>About Us</title>
+  </head>
+  <body>
+  <header>
+        <!-- Header Section -->
+        <div class="header">
+        <h1>Exam Date Sheet Scheduler</h1>
+        <!-- Navigation Bar -->
+        <div class="navbar">
+            <a href="./home.php">Home</a>
+            <a href="about.php">About Us</a>
+        </div>     
+        </div>
+    </header> 
+      <main>
+        <div class="about-container">
+          <div class="main-card">
+            <div class="student-details">
+              <div class="user-img">
+                <img src="./assets/img/ijaz.jpeg" alt="Student Image" />
+              </div>
+              <div class="user-credential">
+                <h4> IJAZ AKRAM </h4>
+                <p>BSCS</p>
+                <p>BC210207180</p>
+                <p class="std-id"></p>
+              </div>
+            </div>
+            <div class="campus-info">
+              <p><span class="campus">Campus Name:</span> Microin Technology</p>
+              <p><span class="city">City:</span> SARGODHA</p>
+            </div>
+          </div>
+        </div>
+      </main>
+<footer>
+    <!-- Footer Section -->
+    <div class="footer">
+        <p>© 2024 Exam Date Sheet Scheduler. All rights reserved.</p>
+    </div> 
+</footer>
+  </body>
+</html>
